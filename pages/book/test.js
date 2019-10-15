d43a1d9b3bbb5a5be6682551dfc5529793807a28
LuJiaWei <747624075@qq.com>
2019-10-15 23:29:34 +0800

    2

--- pages/book/test.js
+++ pages/book/test.js
@@ -11,7 +11,10 @@ Page({
     readTest: {},
     readQuestion: [],
     answer: [],
-    radio: ''
+    radio: '',
+
+    show: false, // 翻译弹窗
+    numberTip: '',
   },
 
   /**
@@ -54,37 +57,48 @@ Page({
           readQuestion: r.data.content.result,
           answer: answer
         });
-        console.log(_this.data)
       }
     });
   },
   submit: function(){
     const _this = this
-    // let answer = _this.data.answer
-    // answer[e.currentTarget.dataset.index] = e.detail
-    // _this.setData({
-    //   answer: answer
-    // });
+    let state = 0
+    _this.data.answer.map(function(e){
+      if (e.answer2 == ''){
+        state++
+      }
+    })
+    if(state != 0){
+      app.tools.toast('请填写完成后再提交···')
+      return
+    }
     app.tools.request({
       url: 'reading/submit',
       method: "POST",
       data: _this.data.answer,
       success: function (r) {
-        // let answer = []
-        // r.data.content.result.map(function (e) {
-        //   answer.push({
-        //     answer2: '',
-        //     reqid: e.id,
-        //   })
-        // })
-        // _this.setData({
-        //   readQuestion: r.data.content.result,
-        //   answer: answer
-        // });
-        console.log(r)
-        // console.log(_this.data)
+        let answerList = []
+        r.data.content.result.details.map(function (e, i) {
+          if (e.ansresult != 'R'){
+            answerList.push({
+              i: i,
+              q: e.readingCpsQuestion.description,
+              a: e.readingCpsQuestion.explain,
+              answer: e.readingCpsQuestion.answer,
+            })
+          }
+        })
+        _this.setData({
+          numberTip: '回答正确数：' + r.data.content.result.result1 + '，回答错误数：' + r.data.content.result.result2,
+          show: true,
+          answerList: answerList,
+        });
+        console.log(_this.data)
       }
     });
+  },
+  dialogClose: function(){
+    wx.navigateBack()
   },
   onClick(event) {
     const _this = this
